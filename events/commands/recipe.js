c82ab62abcb92693798a26d6232c17b824a85dc2
Vincent Boisdon <v.i.boisdon@gmail.com>
2023-12-03 23:28:02 +0000

    Ajout commande recipe

---
+++ events/commands/recipe.js
@@ -0,0 +1,54 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { getRecipesFromIngredients } from '../../lib/test.js';
+
+// Creates an Object in JSON with the data required by Discord's API to create a SlashCommand
+const create = () => {
+	const command = new SlashCommandBuilder()
+		.setName('recipe')
+		.setDescription('Simule une cuisson.')
+		.addStringOption((option) =>
+			option.setName('ing1')
+            .setDescription('Premier ingrédient')
+            .setRequired(true)
+            .setAutocomplete(true)
+		)
+        .addStringOption((option) =>
+			option.setName('ing2')
+            .setDescription('Deuxième ingrédient')
+            .setRequired(true)
+            .setAutocomplete(true)
+		)
+        .addStringOption((option) =>
+			option.setName('ing3')
+            .setDescription('Troisième ingrédient')
+            .setRequired(true)
+            .setAutocomplete(true)
+        )
+        .addStringOption((option) =>
+			option.setName('ing4')
+            .setDescription('Quatrième ingrédient')
+            .setRequired(true)
+            .setAutocomplete(true)
+        );
+
+	return command.toJSON();
+};
+
+// Called by the interactionCreate event listener when the corresponding command is invoked
+const invoke = (interaction) => {
+	const ing1 = interaction.options.getString('ing1');
+	const ing2 = interaction.options.getString('ing2');
+	const ing3 = interaction.options.getString('ing3');
+	const ing4 = interaction.options.getString('ing4');
+
+    let recipes = getRecipesFromIngredients([ing1, ing2, ing3, ing4])
+
+	if (recipes !== null) interaction.reply({ content: `Result is ${recipes[2].name}!` });
+	else
+		interaction.reply({
+			content: 'Pong!',
+			ephemeral: true,
+		});
+};
+
+export { create, invoke };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,29 @@
+import { getFoodItemArray } from '../lib/test.js';
 const once = false;
 const name = 'interactionCreate';
 
 async function invoke(interaction) {
 	// Check if the interaction is a command and call the invoke method in the corresponding file
 	// The #commands ES6 import-abbreviation is defined in the package.json
-	if (interaction.isChatInputCommand())
+	if (interaction.isChatInputCommand()) {
 		(await import(`#commands/${interaction.commandName}`)).invoke(interaction);
+	}
+	if (interaction.isAutocomplete()){
+		if (interaction.commandName === "recipe") { 
+			const focusedOption = interaction.options.getFocused(true);
+			let choices = getFoodItemArray()
+			const filtered = choices.filter(choice => choice.name.toLowerCase().includes(focusedOption.value));
+			let options;
+			if (filtered.length > 25) {
+				options = filtered.slice(0, 25);
+			} else {
+				options = filtered;
+			}
+			await interaction.respond(
+				options,
+			);
+		}
+	}
 }
 
 export { once, name, invoke };
