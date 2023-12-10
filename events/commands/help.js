697cb704b607639457d76cac0b57390446792ea9
Vincent Boisdon <v.i.boisdon@gmail.com>
2023-12-10 18:28:38 +0000

    Added some help options

--- events/commands/help.js
+++ events/commands/help.js
@@ -9,14 +9,48 @@ const helpTopics = {
             {
                 'name': 'General purpose',
                 'value': `Recipes come with a set of requirements, many of which concern tags.
-                            Those are a way to categorize ingredients.
-                            Because it makes sense to be able to replace a fruit by another fruit.
-                            Hence, most of them describe a real life type of food.`
+                            Those are a way to categorize ingredients like fruits or veggies.
+                            Most of them describe a real life type of food.`
+            },
+            {
+                'name': 'Mechanics',
+                'value': `Ingredients sometimes have a value associated with a type.
+                            For example, a green cap is valued as half a veggie.
+                            So if a recipe needs 1 veggie, it'll need 2 green caps.
+                            
+                            Ingredients can also count as multiple of a tag, like a lobster counting for 2 fishes, or a tallbird egg counting as 4 eggs.`
             },
             {
                 'name': 'Specific tags',
                 'value': `Since this is don't starve, ingredients can have a 'monster' tag which will impact many recipes.
                             An ingredient can also be typed as 'magic' or 'inedible'.`
+            }
+        ]
+    },
+    'priorities': {
+        'title': 'Priorities in Don\'t Starve cooking',
+        'thumbnail': 'priorities',
+        'sections': [
+            {
+                'name': 'Base mecanic',
+                'value': `Recipes have a priorities associated to them that can be anything from 100 to -4.
+                            It decides which of the possible outcome of the cooking is gonna be choosed.
+                            Out of the recipes which requirements are met by the ingredients in the crockpot, the one with the highest priority is chosen.`
+            },
+            {
+                'name': 'Same priority case',
+                'value': `In some cases, one or more possible recipes have the exact same priority.
+                            The outcome will be randomly chosen between the recipes.`
+            }
+        ]
+    },
+    'perishing': {
+        'title': 'Perishing in Don\'t Starve cooking',
+        'thumbnail': 'rot',
+        'sections': [
+            {
+                'name': 'To be Filled',
+                'value': `No idea what to write.`
             }
         ]
     }
