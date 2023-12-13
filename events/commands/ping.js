f1cad0cea70e56fc597b724e5727fea1e3e85f16
Vincent Boisdon <v.i.boisdon@gmail.com>
2023-12-13 22:48:32 +0000

    diverse stuff

--- events/commands/ping.js
+++ events/commands/ping.js
@@ -1,10 +1,11 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { test } from '#lib/tools'
 
 // Creates an Object in JSON with the data required by Discord's API to create a SlashCommand
 const create = () => {
 	const command = new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replys with Pong!')
+		.setDescription('Replies with Pong!')
 		.addUserOption((option) =>
 			option.setName('user').setDescription('Shall I greet a user?')
 		);
@@ -14,6 +15,7 @@ const create = () => {
 
 // Called by the interactionCreate event listener when the corresponding command is invoked
 const invoke = (interaction) => {
+	test()
 	const user = interaction.options.getUser('user');
 
 	if (user !== null) interaction.reply({ content: `Hey ${user}!` });
